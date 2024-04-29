0bdf92aa9844dd2021e01be5d349bc8ddf509327
kush <kuspatel36@gmail.com>
2024-04-29 16:58:01 +0530

    i updated in routes in product.js file

--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,12 @@ const express = require('express')
 const productController = require('../Controllers/product')
 const router = express.Router()
 
-router.get('/products', productController.getAllProduct)
-    .get('/products/:id', productController.getProduct)
-    .post('/products', productController.createProduct)
-    .put('/products/:id', productController.replaceProduct)
-    .patch('/products/:id', productController.updateProduct)
-    .delete('/products/:id', productController.deleteProduct)
+router.get('/', productController.getAllProduct)
+    .get('/:id', productController.getProduct)
+    .post('/', productController.createProduct)
+    .put('/:id', productController.replaceProduct)
+    .patch('/:id', productController.updateProduct)
+    .delete('/:id', productController.deleteProduct)
 
 exports.routes = router;
 
